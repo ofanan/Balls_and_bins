8e0ba05dc59fd88a9bbc3a9b4f6cea63bc64c112
ofanan <ofanan@gmail.com>
2024-07-08 20:41:45 +0300

    Initial commit from PC.

---
+++ src/Balls_and_bins.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : Balls_and_bins.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+#include "settings.h"
+
+//#include <settings.h>
+//using namespace settings;
+using namespace std;
+
+//if ( std::find(vec.begin(), vec.end(), item) != vec.end() )
+
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
+	Verbose_t verbose[2]; // = {DEBUG, DEBUG};
+//	if (verbose==DEBUG) {
+//		cout << "DEBUG";
+//	}
+//	int VERBOSE_RES = 5;
+//	cout << VERBOSE_RES << endl;
+	return 0;
+}
---
+++ src/settings.h
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+enum Verbose_t {RES, LOG, DEBUG};
+
+//const int VERBOSE_RES = 1;
+//namespace settings
+//{
+//	const int VERBOSE_RES = 1;
+//}
+//
